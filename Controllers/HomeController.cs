eac1366e8a83a53d43435edb60d4df64cbaeee62
leontievasophia <leontievasophia@gmail.com>
2026-03-24 22:52:15 +0200

    Add ASP.NET MVC project with MysticHub functionality

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using group_project.Models;
+
+namespace group_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var predictionService = new Prediction();
+            string prediction = predictionService.GetRandomPrediction();
+
+            ViewBag.PredictionText = prediction;
+
+            return View();
+        }
+    }
+}
