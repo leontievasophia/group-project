7b8e90963b59490c05774a626b025f77d7ca2fc9
Andriana Koblianska <andriana.k2712@gmail.com>
2026-04-16 20:28:21 +0300

    Add horoscope info model

---
+++ Models/HoroscopeInfo.cs
@@ -0,0 +1,10 @@
+namespace group_project.Models
+{
+    public class HoroscopeInfo
+    {
+        public string SignKey { get; set; }
+        public string SignName { get; set; }
+        public string ImagePath { get; set; }
+        public string HoroscopeText { get; set; }
+    }
+}
