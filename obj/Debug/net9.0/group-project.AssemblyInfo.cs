ea5029485f6b2bcd089a8aa60e596cc4b03871b2
Andriana Koblianska <andriana.k2712@gmail.com>
2026-05-20 20:25:13 +0300

    Added compatibility page

--- obj/Debug/net9.0/group-project.AssemblyInfo.cs
+++ obj/Debug/net9.0/group-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("group-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d987dc68e76d3ed10f8c01d7c4beb81848bff455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9dc2cad6ab6ad9e29cbaf18f7451df81642740")]
 [assembly: System.Reflection.AssemblyProductAttribute("group-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("group-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
